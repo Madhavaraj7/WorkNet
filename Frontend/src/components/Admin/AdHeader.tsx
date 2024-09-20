fe4cf69dc902c3c8128e78bc9ad79b5ca078eaa7
Madhavaraj R <raj266864@gmail.com>
2024-09-20 16:13:31 +0530

    host

--- Frontend/src/components/Admin/AdHeader.tsx
+++ Frontend/src/components/Admin/AdHeader.tsx
@@ -26,8 +26,8 @@ import { io } from "socket.io-client";
 import { getUnreadMessagesCount } from "../../Services/allAPI";
 
 // Initialize socket connection
-const socket = io("https://worknet.onrender.com");
-// const socket = io("http://localhost:3000"); // for localhost
+// const socket = io("https://worknet.onrender.com");
+const socket = io("http://localhost:3000"); // for localhost
 
 
 const drawerWidth = 240;
