23eeaa8f6f48c4bf0e2739a1e29a513aa0b1289a
Madhavaraj R <raj266864@gmail.com>
2024-10-16 15:46:08 +0530

    updated

--- Frontend/src/Services/serverURL.ts
+++ Frontend/src/Services/serverURL.ts
@@ -1,3 +1,3 @@
-export const SERVER_URL = "https://worknet.solutions/api/users"; //for production
+// export const SERVER_URL = "https://worknet.solutions/api/users"; //for production
 
-// export const SERVER_URL = "http://localhost:3000/api/users";
+export const SERVER_URL = "http://localhost:3000/api/users";
